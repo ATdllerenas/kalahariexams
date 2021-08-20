eb30d0a781aef2d56794c0d859d3b96f2856a108
Anzen <dayana.llerenas@agilethought.com>
2021-08-20 13:25:20 -0500

    Create Proyect Exam Assignment for Backend

---
+++ src/main/java/mx/com/agile/academia/kalahariexams/KalahariexamsApplication.java
@@ -0,0 +1,13 @@
+package mx.com.agile.academia.kalahariexams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalahariexamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalahariexamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/agile/academia/kalahariexams/KalahariexamsApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.agile.academia.kalahariexams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalahariexamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
